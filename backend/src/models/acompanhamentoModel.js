dfc0db60b6522b97201c814d62a5d1da9cbe9d67
Paulo Henrique <paulo_phs01@hotmail.com>
2023-10-19 22:39:55 -0300

    endpoint acompanhamento trazendo altura

--- backend/src/models/acompanhamentoModel.js
+++ backend/src/models/acompanhamentoModel.js
@@ -2,7 +2,7 @@ const connection = require('../services/connection');
 
 const getAcompanhamentoFromAluno = async (id) => {
     try{
-        const acompanhamento = await connection.execute('SELECT pesoAtual from aula WHERE alunoID = ?',[id]);
+        const acompanhamento = await connection.execute('SELECT a.pesoAtual, al.altura from aula a LEFT JOIN aluno al on al.alunoID = a.alunoID WHERE a.alunoID = ?',[id]);
         return acompanhamento;
     } catch (error) {
         return { error: `Failed to retrieve students: ${error.message}` };
